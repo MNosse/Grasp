857004e80364897d2c1c32034b59c61d51674275
Mateus Nosse <80108925+MNosse@users.noreply.github.com>
2022-07-23 14:46:26 -0300

    Update GRASP+BLSPM.py

--- GRASP+BLSPM.py
+++ GRASP+BLSPM.py
@@ -67,8 +67,13 @@ def grasp(time_limit):
     time_limit = time.time() + time_limit
     solucao_incumbente = [0] * n
     while time.time() < time_limit:
+        print('h')
         nova_solucao = heuristica_construtiva()
+        print('nova Solucao: ' + str(nova_solucao)[1:-1] + ", Custo: " + str(calcular_custo(nova_solucao)))
+        print('>')
         nova_solucao = busca_local_simples_primeira_melhora(nova_solucao, time_limit_BLSPM)
+        print('<')
+        print('nova Solucao: ' + str(nova_solucao)[1:-1] + ", Custo: " + str(calcular_custo(nova_solucao)))
         if calcular_custo(nova_solucao) > calcular_custo(solucao_incumbente):
             solucao_incumbente = nova_solucao.copy()
             print('Solucao: ' + str(solucao_incumbente)[1:-1] + ", Custo: " + str(calcular_custo(solucao_incumbente)))
